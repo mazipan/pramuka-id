ae11639d3e78cedd21f68899458104fea46fa0df
Miftahul Lubab <miftahul.lubab20@gmail.com>
2020-10-08 11:15:44 +0700

    Change speaker icon color when Speech API is used and change cursor style when card radio code is hovered

--- pages/radio.js
+++ pages/radio.js
@@ -6,6 +6,7 @@ import ChapterTitle from '../components/ChapterTitle';
 import RadioData from '../data/radio';
 import { FaVolumeUp } from 'react-icons/fa'
 import { BASE_PATH } from '../constants';
+import { useState } from 'react';
 
 const title = 'Kode Radio Lengkap | Buku Saku Pramuka Digital';
 const desc = 'Daftar lengkap kode radio';
@@ -14,11 +15,14 @@ const url = BASE_PATH + '/radio/';
 function LambangPramukaPage() {
 
   const { canSpeak } = useSpeechAPI();
+  const [ isSpeak, setIsSpeak ] = useState('');
 
   const playAudio = (word) => {
     try {
       let utterance = new SpeechSynthesisUtterance(word);
       utterance.lang = "en-US"
+      utterance.onstart = () => setIsSpeak(word); //when starting to play audio
+      utterance.onend = () => setIsSpeak(''); //when finished playing audio
       speechSynthesis.speak(utterance);
     } catch (error) {
       console.error(error)
@@ -41,12 +45,12 @@ function LambangPramukaPage() {
                   <div
                     key={item.text}
                     onClick={() => canSpeak && playAudio(item.code.toLowerCase())}
-                    className="w-1/2 md:w-1/5 flex items-center rounded overflow-hidden shadow-lg"
+                    className="w-1/2 md:w-1/5 flex items-center rounded overflow-hidden shadow-lg cursor-pointer"
                   >
                     <div className="px-2 py-2 flex items-center w-full">
                       <div className="w-10">{item.text}</div>{' '}
                       <div className="text-orange-500">{item.code}</div>
-                      {canSpeak && <div className="ml-auto text-gray-500 cursor-pointer"><FaVolumeUp/></div>}
+                      {canSpeak && <div className={ (isSpeak === item.code.toLowerCase()) ? "ml-auto text-orange-900" : "ml-auto text-gray-500" }><FaVolumeUp/></div>}
                     </div>
                   </div>
                 ))}
