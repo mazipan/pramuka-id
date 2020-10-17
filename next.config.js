0e3d02fb14dd07e3186f36c55638f453809c9a83
adib <dib.firman@gmail.com>
2020-10-17 20:15:59 +0700

    chore: trying migrate to preact

--- next.config.js
+++ next.config.js
@@ -1,3 +1,43 @@
-module.exports = {
+const withPrefesh = require('@prefresh/next')
+
+const configs = {
   poweredByHeader: false,
+
+  webpack(config, { dev, isServer }) {
+    const splitChunks = config.optimization && config.optimization.splitChunks
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups;
+      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test: preactModules
+        });
+        cacheGroups.commons.name = 'framework';
+      }
+      else {
+        cacheGroups.preact = {
+          name: 'commons',
+          chunks: 'all',
+          test: preactModules
+        };
+      }
+    }
+    
+    // Install webpack aliases:
+    const aliases = config.resolve.alias || (config.resolve.alias = {});
+    aliases.react = aliases['react-dom'] = 'preact/compat';
+
+    // inject Preact DevTools
+    if (dev && !isServer) {
+      const entry = config.entry;
+      config.entry = () => entry().then(entries => {
+        entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
+        return entries;
+      });
+    }
+
+    return config;
+  }
 }
+
+module.exports = withPrefesh(configs)
