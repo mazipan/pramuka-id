1bff0e93f608f9023b9eb3891a5d13a43fc5ae24
Sunny Praksah <38065786+sprakash57@users.noreply.github.com>
2020-10-19 06:26:07 +0530

    fix(mainlayout.js): fixed: Dark mode didn't change the theme meta tag (#69)

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -10,7 +10,7 @@ const desc =
   'Buku saku pramuka digital dalam bentuk web, ✅ GRATIS, ✅ tanpa iklan, ❌ tidak perlu install Aplikasi lagi';
 const url = BASE_PATH;
 const metaImg = BASE_PATH + '/assets/5930.jpg';
-const authorName =  'Irfan Maulana';
+const authorName = 'Irfan Maulana';
 
 function Layout(props) {
   const [appTheme, setAppTheme] = React.useState('light');
@@ -39,10 +39,10 @@ function Layout(props) {
         <meta charSet="utf-8" />
         <meta httpEquiv="content-type" content="text/html" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <meta name="theme-color" content="#7B341E" />
+        <meta name="theme-color" content={appTheme === "light" ? "#7B341E" : "#101623"} />
         <title key="title">{title}</title>
-        
-        <meta name="author" content={authorName} />  
+
+        <meta name="author" content={authorName} />
         <meta key="description" name="description" content={desc} />
         <meta name="keywords" content="Pramuka, Buku Saku, Buku Saku Digital" />
         <meta name="robots" content="index, follow" />
@@ -120,11 +120,11 @@ function Layout(props) {
             toggleTheme()
           }}
         >
-          <div id="light" className={appTheme === 'light' ? 'hidden' : null } style={{ width: '2rem', height: '2rem' }}>
-            <svg xmlns='http://www.w3.org/2000/svg' className='ionicon' viewBox='0 0 512 512'><title>Sunny</title><path fill='none' stroke='currentColor' strokeLinecap='round' strokeMiterlimit='10' strokeWidth='32' d='M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94'/><circle cx='256' cy='256' r='80' fill='none' stroke='currentColor' strokeLinecap='round' strokeMiterlimit='10' strokeWidth='32'/></svg>
+          <div id="light" className={appTheme === 'light' ? 'hidden' : null} style={{ width: '2rem', height: '2rem' }}>
+            <svg xmlns='http://www.w3.org/2000/svg' className='ionicon' viewBox='0 0 512 512'><title>Sunny</title><path fill='none' stroke='currentColor' strokeLinecap='round' strokeMiterlimit='10' strokeWidth='32' d='M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94' /><circle cx='256' cy='256' r='80' fill='none' stroke='currentColor' strokeLinecap='round' strokeMiterlimit='10' strokeWidth='32' /></svg>
           </div>
-          <div id="moon" className={appTheme === 'dark' ? 'hidden' : null } style={{ width: '2rem', height: '2rem' }}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512"><title>Moon</title><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/></svg>
+          <div id="moon" className={appTheme === 'dark' ? 'hidden' : null} style={{ width: '2rem', height: '2rem' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512"><title>Moon</title><path d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" /></svg>
           </div>
         </button>
       </div>
