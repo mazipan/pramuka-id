951cfb92d271387e875ee030eb8f4236bd326231
Herka Trya Fitra <herkahahaha@gmail.com>
2020-10-28 02:54:03 +0700

    add font & base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,36 @@
 module.exports = {
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  purge: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#1a1c20",
+        primary: "#7B341E",
+        secound: {
+          1: "#87431d",
+          2: "#c87941",
+        },
+        light: "#f4f4f4",
+        soft: "#dbcbbd",
+      },
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
     inset: {
-      '0': 0,
-      '1/2': '50%',
-		},
-		minWidth: {
-			'80': '80px',
-			'150': '150px',
-			'250': '250px'
-		},
-		minHeight: {
-			'80': '80px'
-		},
-		listStyleType: {
-			'lower-alpha': 'lower-alpha',
-		}
+      0: 0,
+      "1/2": "50%",
+    },
+    minWidth: {
+      80: "80px",
+      150: "150px",
+      250: "250px",
+    },
+    minHeight: {
+      80: "80px",
+    },
+    listStyleType: {
+      "lower-alpha": "lower-alpha",
+    },
   },
   variants: {},
   plugins: [],
