5150032b37f854de4076b7d815dc917dc4a43ea0
Irfan Maulana <mazipanneh@gmail.com>
2021-07-29 08:00:52 +0700

    chore: update brand title

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH
 
 export const BASE_PATH = basePath || ''
-export const BRAND_TITLE = 'Buku Saku Pramuka'
+export const BRAND_TITLE = 'Pramuka.Online'
 export const BRAND_LONG_DESC =
   'Kumpulan informasi digital mengenai berbagai materi pramuka, materi kenegaraan dan materi kecakapan umum'
