5420c973c3ef25653e45406f8b5649d75af4e78c
Irfan Maulana <mazipanneh@gmail.com>
2021-08-14 10:06:18 +0700

    Update SeoText.tsx

--- src/components/SeoText.tsx
+++ src/components/SeoText.tsx
@@ -4,7 +4,7 @@ interface SeoTextProps {
 
 function SeoText({ text }: SeoTextProps) {
   return (
-    <div className="mt-6 text-sm text-gray-400">
+    <div className="mt-6 text-sm text-gray-600 dark:text-gray-100">
       <p>{text}</p>
     </div>
   )
